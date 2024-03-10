10987eb10c0674799e9879ed84313138cc80b95c
Asia <asia@zipcodersmbp18.mynetworksettings.com>
2024-03-10 13:53:41 -0400

    get words method created

---
+++ src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -0,0 +1,24 @@
+package com.github.zipcodewilmington;
+import java.util.Arrays;
+import java.util.Random;
+
+
+
+public class WordGuess {
+ // Instance variable for Hangman game is an array of words for the player to guess
+    public String [] words = {"cheese", "run", "dance", "play"};
+
+
+
+  // Generate a word for the player to guess
+public StringBuilder getWords(String[] n) {
+    int random_Num = words(0.words.length-1);
+    StringBuilder randomWord = new StringBuilder();
+    randomWord.append(random_Num);
+    return randomWord;
+
+}
+
+
+
+}
