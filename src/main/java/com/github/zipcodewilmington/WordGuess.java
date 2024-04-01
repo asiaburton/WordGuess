f1258aa3c6d4c4fb5d62e94f4ce163f638c06774
Asia <asia@Zipcoders-MacBook-Pro-18.local>
2024-04-01 07:27:33 -0400

    Character array possibilitiy, switching to single character String array

--- src/main/java/com/github/zipcodewilmington/WordGuess.java
+++ src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -1,4 +1,5 @@
 package com.github.zipcodewilmington;
+import javax.xml.stream.events.Characters;
 import java.sql.PreparedStatement;
 import java.util.Arrays;
 import java.util.Random;
@@ -7,32 +8,70 @@ import java.util.Scanner;
 
 public class WordGuess {
 
- public static void main (String[] args) {
-     Scanner scanner = new Scanner(System.in);
-     Random random = new Random();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
 
-    String[] wordlist = {"cheese", "run", "dance", "play"};
-    Integer randomNum = random.nextInt(wordlist.length);
-    String secretWord = wordlist[randomNum];
-    this.secretWord = secretWord.toCharArray();
+        String[] wordlist = {"cheese", "run", "dance", "play"};
+        Integer randomNum = random.nextInt(wordlist.length);
+        String secretWord = wordlist[randomNum];
+        char[] guess = new char[secretWord.length()];
+        char[] solution = secretWord.toCharArray();
+        int triesAllowed = secretWord.length();
+        int tries = 0;
+        int triesRemaining = triesAllowed - tries;
+
+        System.out.println("Let's Play Wordguess version 1.0! " +
+                "\nTry to guess the secret word by entering each letter... \n " +
+                "Current Guesses: " + triesAllowed);
 
 
-    char[] guess = new char[secretWord.toCharArray().length];
-    for(int i = 0; i <secretWord.toCharArray().length; i++) {
-        guess[i] = '_';
+        while ((tries < triesAllowed) && (!(guess == solution))) {
+            for (int i = 0; i < guess.length; i++) {
+                guess[i] = '_';
+                System.out.println(guess);
+                System.out.println("Enter a single character: ");
+                String letter = scanner.next();
+
+
+                if ((new String(solution).contains(letter))) {
+                    // place correct guessed letter into guessed char array at correct index
+                    int correctGuess = new String(solution).indexOf(letter);
+                    letter = String.valueOf(guess[correctGuess]);
+                    tries++;
+                    System.out.println("You have " + triesRemaining + " tries left.");
+                } else if
+                    // guessed char is not in solution, line does not change
+                (!(new String(solution).contains(letter))) {
+                    tries++;
+                    System.out.println("You have " + triesRemaining + " tries left.");
+
+                } else if
+                    // guessed char == '-' exit game.
+                (letter.equals('-')) {
+                    System.out.println("Goodbye for now!");
+                    break;
+                }
+
+
+                 //System.out.println(guess);
+                //System.out.println(solution);
+
+
+            }
+        }
+
+        }
     }
 
 
 
-     }
-
-
- }
 
 
 
 
 
 
-}
+
+
